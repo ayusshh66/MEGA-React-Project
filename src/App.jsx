ba1b705c93045e2357ef320d76fde84def034a12
ayusshh66 <adonisjefrey66@gmail.com>
2026-04-04 20:31:53 +0530

    MEGA react project day1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+  return (
+    <>
+      <h1>hi my name is ayush </h1>
+    </>
+  )
+}
+
+export default App
